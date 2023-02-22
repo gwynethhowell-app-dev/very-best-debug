584cf04e79e98df1b2fa130d860238697831d978
gwynethhowell <ghowell@uchicago.edu>
2023-02-22 05:26:38 +0000

    working debug

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get("/users", { :controller => "user", :action => "all_users" })
   get("/users/:username", { :controller => "user", :action => "show" })
   get("/insert_user_record", { :controller => "user", :action => "create" })
-  get("/update_users/:user_id", { :controller => "user", :action => "update" })
+  get("/update_user/:user_id", { :controller => "user", :action => "update" })
 
   get("/venues", { :controller => "venues", :action => "index" })
   get("/insert_venue_record", { :controller => "venues", :action => "create" })
